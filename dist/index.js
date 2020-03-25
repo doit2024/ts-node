33eeac60183314fb00d6136a8fe55ec3405e4dde
niudazhuang <dengwm666@icloud.com>
2020-03-25 18:36:27 +0800

    del

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var user_1 = require("./user");
-var user = new user_1.User('Jon', 'Snow');
-console.log("I am " + user.getFullName());
-//# sourceMappingURL=index.js.map
